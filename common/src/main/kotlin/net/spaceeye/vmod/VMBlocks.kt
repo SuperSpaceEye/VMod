f73f6c1bbc661bc5bb78f59950e336804b6ed706
Logan <brickmasterlogan@icloud.com>
2024-04-01 18:14:10 -0400

    Creative Tab fixed

--- common/src/main/kotlin/net/spaceeye/vmod/VMBlocks.kt
+++ common/src/main/kotlin/net/spaceeye/vmod/VMBlocks.kt
@@ -1,23 +1,23 @@
 package net.spaceeye.vmod
 
 import dev.architectury.registry.registries.DeferredRegister
-import net.minecraft.client.resources.model.Material
-import net.minecraft.core.Registry
+import dev.architectury.registry.registries.RegistrySupplier
 import net.minecraft.core.registries.Registries
 import net.minecraft.world.item.BlockItem
 import net.minecraft.world.item.Item
 import net.minecraft.world.level.block.state.BlockBehaviour
 import net.spaceeye.vmod.blocks.SimpleMessager
+import net.spaceeye.vmod.VMItems.TAB
 
 object VMBlocks {
     private val BLOCKS = DeferredRegister.create(VM.MOD_ID, Registries.BLOCK)
 
-    var SIMPLE_MESSAGER = BLOCKS.register("simple_messager") { SimpleMessager(BlockBehaviour.Properties.of().strength(2.0f)) }
+    var SIMPLE_MESSAGER: RegistrySupplier<SimpleMessager> = BLOCKS.register("simple_messager") { SimpleMessager(BlockBehaviour.Properties.of().strength(2.0f)) }
 
     fun register() {BLOCKS.register()}
     fun registerItems(items: DeferredRegister<Item?>) {
         for (block in BLOCKS) {
-            items.register(block.id) { BlockItem(block.get(), Item.Properties()) }
+            items.register(block.id) { BlockItem(block.get(), Item.Properties().`arch$tab`(TAB)) }
         }
     }
 }

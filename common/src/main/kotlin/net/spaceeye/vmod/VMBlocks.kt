6bddf149381e978a377e7a11a6fb09079e9c2afc
spaceeye <superspaceeye@gmail.com>
2024-07-13 19:07:21 +0300

    fix

--- common/src/main/kotlin/net/spaceeye/vmod/VMBlocks.kt
+++ common/src/main/kotlin/net/spaceeye/vmod/VMBlocks.kt
@@ -9,20 +9,17 @@ import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.state.BlockBehaviour
 import net.spaceeye.vmod.blocks.SimpleMessager
 import net.spaceeye.vmod.VMItems.TAB
+import java.util.function.Supplier
 
 object VMBlocks {
     private val BLOCKS = DeferredRegister.create(VM.MOD_ID, Registries.BLOCK)
 
-    private val dontRegister = mutableSetOf<Block>()
-
     var SIMPLE_MESSAGER: RegistrySupplier<SimpleMessager> = BLOCKS.register("simple_messager") { SimpleMessager(BlockBehaviour.Properties.of().strength(2.0f)) }
-    var CONE_THRUSTER: RegistrySupplier<Block> = BLOCKS.register("cone_thruster") { noRegister(Block(BlockBehaviour.Properties.of().strength(2.0f))) }
+    var CONE_THRUSTER: RegistrySupplier<Block> = BLOCKS.register("cone_thruster") { Block(BlockBehaviour.Properties.of().strength(2.0f)) }
 
-    private fun <T: Block>noRegister(block: T): T { dontRegister.add(block); return block}
     fun register() {BLOCKS.register()}
     fun registerItems(items: DeferredRegister<Item?>) {
         for (block in BLOCKS) {
-            if (dontRegister.contains(block.get())) {continue}
             items.register(block.id) { BlockItem(block.get(), Item.Properties().`arch$tab`(TAB)) }
         }
     }

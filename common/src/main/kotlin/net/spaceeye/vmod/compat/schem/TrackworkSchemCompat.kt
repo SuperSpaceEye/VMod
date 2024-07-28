9c8ab341dda1414845a42f287d9ac2614e621e90
spaceeye <superspaceeye@gmail.com>
2024-07-28 15:38:12 +0300

    fix

--- common/src/main/kotlin/net/spaceeye/vmod/compat/schem/TrackworkSchemCompat.kt
+++ common/src/main/kotlin/net/spaceeye/vmod/compat/schem/TrackworkSchemCompat.kt
@@ -4,6 +4,7 @@ import edn.stratodonut.trackwork.TrackBlocks
 import net.minecraft.core.BlockPos
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.server.level.ServerLevel
+import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 import org.valkyrienskies.core.api.ships.ServerShip
@@ -11,19 +12,22 @@ import org.valkyrienskies.core.api.ships.ServerShip
 class TrackworkSchemCompat: SchemCompatItem {
     override fun onCopy(level: ServerLevel, pos: BlockPos, state: BlockState, ships: List<ServerShip>, be: BlockEntity?, tag: CompoundTag?, cancelBlockCopying: () -> Unit) {}
 
-    private val blocksSet = setOf(
-         TrackBlocks.LARGE_SUSPENSION_TRACK.get()
-        ,TrackBlocks.MED_SUSPENSION_TRACK.get()
-        ,TrackBlocks.SUSPENSION_TRACK.get()
-        ,TrackBlocks.LARGE_PHYS_TRACK.get()
-        ,TrackBlocks.MED_PHYS_TRACK.get()
-        ,TrackBlocks.PHYS_TRACK.get()
-        ,TrackBlocks.SIMPLE_WHEEL.get()
-        ,TrackBlocks.SIMPLE_WHEEL_PART.get()
-    )
+    private var blocksSet: Set<Block>? = null
 
     override fun onPaste(level: ServerLevel, oldToNewId: Map<Long, Long>, tag: CompoundTag, state: BlockState, delayLoading: () -> Unit, afterPasteCallbackSetter: ((be: BlockEntity?) -> Unit) -> Unit) {
-        if (!blocksSet.contains(state.block)) return
+        if (blocksSet == null) {
+            blocksSet = setOf(
+                TrackBlocks.LARGE_SUSPENSION_TRACK.get()
+                ,TrackBlocks.MED_SUSPENSION_TRACK.get()
+                ,TrackBlocks.SUSPENSION_TRACK.get()
+                ,TrackBlocks.LARGE_PHYS_TRACK.get()
+                ,TrackBlocks.MED_PHYS_TRACK.get()
+                ,TrackBlocks.PHYS_TRACK.get()
+                ,TrackBlocks.SIMPLE_WHEEL.get()
+                ,TrackBlocks.SIMPLE_WHEEL_PART.get()
+            )
+        }
+        if (!blocksSet!!.contains(state.block)) return
         tag.putBoolean("Assembled", false)
         tag.remove("trackBlockID")
     }

e8503089d2df03a0cf1cfa6c1b8e46f4e5736e50
spaceeye <superspaceeye@gmail.com>
2025-02-11 20:49:58 +0300

    some interesting ideas but no progress on mass sync + removed groundId

---
+++ common/src/main/kotlin/net/spaceeye/vmod/vsStuff/SynchronisedShipMassTracker.kt
@@ -0,0 +1,13 @@
+package net.spaceeye.vmod.vsStuff
+
+import org.valkyrienskies.core.api.ships.properties.ShipId
+
+object SynchronisedShipMassTracker {
+    fun addAllToWatch(shipId: ShipId, mass: Double) {
+
+    }
+
+    fun removeAllFromWatch(shipId: ShipId) {
+
+    }
+}
--- common/src/main/kotlin/net/spaceeye/vmod/vsStuff/VSJointsTracker.kt
+++ common/src/main/kotlin/net/spaceeye/vmod/vsStuff/VSJointsTracker.kt
@@ -4,6 +4,7 @@ import net.spaceeye.vmod.ELOG
 import net.spaceeye.vmod.events.AVSEvents
 import net.spaceeye.vmod.mixin.ShipObjectWorldAccessor
 import net.spaceeye.vmod.utils.MyConnectivityInspector
+import net.spaceeye.vmod.utils.SafeEventEmitter
 import net.spaceeye.vmod.utils.ServerLevelHolder
 import org.jetbrains.annotations.ApiStatus
 import org.jgrapht.graph.DefaultEdge
@@ -17,10 +18,14 @@ import org.valkyrienskies.mod.common.shipObjectWorld
 
 object VSJointsTracker {
     private val graph = DefaultListenableGraph(AsSynchronizedGraph(Multigraph<Long, DefaultEdge>(DefaultEdge::class.java)))
-    private val inspector = MyConnectivityInspector(graph) //todo this is optimal
+    private val inspector = MyConnectivityInspector(graph) //todo this is not optimal
     private val edgeInfo = mutableMapOf<Set<Long>, Int>()
-    //is needed so that connectivity doesn't connect ships constrained to ground
-    private var groundId = -1L
+
+    val connectionAdded   = SafeEventEmitter<OnConnectionAdded>()
+    val connectionRemoved = SafeEventEmitter<OnConnectionRemoved>()
+
+    data class OnConnectionAdded  (val shipId1: ShipId, val shipId2: ShipId)
+    data class OnConnectionRemoved(val shipId1: ShipId, val shipId2: ShipId)
 
     init {
         graph.addGraphListener(inspector)
@@ -51,40 +56,34 @@ object VSJointsTracker {
 
 
     @JvmStatic
-    private fun screenShipId(shipId: ShipId?): ShipId {
-        if (shipId == null) {return groundId--}
-        val dimensionIds = ServerLevelHolder.shipObjectWorld!!.dimensionToGroundBodyIdImmutable.values
-        if (dimensionIds.contains(shipId)) {return groundId--}
+    private fun screenShipId(shipId: ShipId?): ShipId? {
+        if (shipId == null || ServerLevelHolder.shipObjectWorld!!.dimensionToGroundBodyIdImmutable.values.contains(shipId)) {return null}
         return shipId
     }
 
     @JvmStatic
     @ApiStatus.Internal
     fun onCreateNewConstraint(joint: VSJoint) {
-        val shipId1 = screenShipId(joint.shipId0)
-        val shipId2 = screenShipId(joint.shipId1)
+        val shipId1 = screenShipId(joint.shipId0) ?: return
+        val shipId2 = screenShipId(joint.shipId1) ?: return
         val pair = setOf(shipId1, shipId2)
 
         var count = edgeInfo.getOrPut(pair) { 0 }
+        if (count == 0) {
+            graph.addVertex(shipId1)
+            graph.addVertex(shipId2)
+            graph.addEdge(shipId1, shipId2)
+            connectionAdded.emit(OnConnectionAdded(shipId1, shipId2))
+        }
         edgeInfo[pair] = count + 1
-
-        graph.addVertex(shipId1)
-        graph.addVertex(shipId2)
-        graph.addEdge(shipId1, shipId2)
     }
 
     @JvmStatic
     @ApiStatus.Internal
     fun onUpdateConstraint(id: Int, oldJoint: VSJoint?, newJoint: VSJoint) {
         if (oldJoint == null) {return onCreateNewConstraint(newJoint)}
+        if (oldJoint.shipId0 == newJoint.shipId0 && oldJoint.shipId1 == newJoint.shipId1) {return}
 
-        val shipId1O = screenShipId(oldJoint.shipId0)
-        val shipId2O = screenShipId(oldJoint.shipId1)
-
-        val shipId1N = screenShipId(newJoint.shipId0)
-        val shipId2N = screenShipId(newJoint.shipId1)
-
-        if (shipId1O == shipId1N && shipId2O == shipId2N) {return}
         onRemoveConstraint(id, oldJoint)
         onCreateNewConstraint(newJoint)
     }
@@ -93,8 +92,8 @@ object VSJointsTracker {
     @ApiStatus.Internal
     fun onRemoveConstraint(id: Int, joint: VSJoint?) {
         val joint = joint ?: return
-        val shipId1 = screenShipId(joint.shipId0)
-        val shipId2 = screenShipId(joint.shipId1)
+        val shipId1 = screenShipId(joint.shipId0) ?: return
+        val shipId2 = screenShipId(joint.shipId1) ?: return
         val pair = setOf(shipId1, shipId2)
 
         var count = edgeInfo.getOrElse(pair) { ELOG("SHOULDN'T BE POSSIBLE. HOW!!!!!!!!!"); return } - 1
@@ -103,5 +102,6 @@ object VSJointsTracker {
 
         edgeInfo.remove(pair)
         graph.removeEdge(shipId1, shipId2)
+        connectionRemoved.emit(OnConnectionRemoved(shipId1, shipId2))
     }
 }

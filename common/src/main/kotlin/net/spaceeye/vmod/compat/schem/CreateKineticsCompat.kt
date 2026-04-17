8942555a0eb4dcb736b6e7c448f081dd09fc6319
SuperSpaceEye <superspaceeye@gmail.com>
2026-04-18 00:08:49 +0300

    * temp remove create

--- common/src/main/kotlin/net/spaceeye/vmod/compat/schem/CreateKineticsCompat.kt
+++ common/src/main/kotlin/net/spaceeye/vmod/compat/schem/CreateKineticsCompat.kt
@@ -1,7 +1,7 @@
 package net.spaceeye.vmod.compat.schem
 
-import com.simibubi.create.content.kinetics.base.KineticBlockEntity
-import com.simibubi.create.foundation.block.IBE
+//import com.simibubi.create.content.kinetics.base.KineticBlockEntity
+//import com.simibubi.create.foundation.block.IBE
 import net.minecraft.core.BlockPos
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.server.level.ServerLevel
@@ -14,14 +14,14 @@ class CreateKineticsCompat: SchemCompatItem {
     override fun onCopy(level: ServerLevel, pos: BlockPos, state: BlockState, ships: List<ServerShip>, centerPositions: Map<Long, JVector3d>, be: BlockEntity?, tag: CompoundTag?, cancelBlockCopying: () -> Unit) {}
 
     override fun onPaste(level: ServerLevel, oldToNewId: Map<Long, Long>, centerPositions: Map<Long, Pair<JVector3d, JVector3d>>, tag: CompoundTag, pos: BlockPos, state: BlockState, tagTransformer: (((CompoundTag?) -> CompoundTag?)?) -> Unit, afterPasteCallbackSetter: ((BlockEntity?) -> Unit) -> Unit) {
-        if (state.block !is IBE<*>) {return}
-        tagTransformer { tag ->
-            level.getBlockEntity(pos) as? KineticBlockEntity ?: return@tagTransformer tag
-            tag?.remove("Speed")
-            tag?.remove("Sequence")
-            tag?.remove("Source")
-            tag?.remove("Network")
-            tag
-        }
+//        if (state.block !is IBE<*>) {return}
+//        tagTransformer { tag ->
+//            level.getBlockEntity(pos) as? KineticBlockEntity ?: return@tagTransformer tag
+//            tag?.remove("Speed")
+//            tag?.remove("Sequence")
+//            tag?.remove("Source")
+//            tag?.remove("Network")
+//            tag
+//        }
     }
 }
